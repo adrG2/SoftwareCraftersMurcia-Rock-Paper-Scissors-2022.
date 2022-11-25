c4a727a6fa7010541d4f5e51876856e8b0b4b821
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-25 19:19:32 +0100

    chore: parametrize test for all the use cases

--- kotlin/src/test/kotlin/RockPaperScissorsTest.kt
+++ kotlin/src/test/kotlin/RockPaperScissorsTest.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
 
 
 enum class GameState {
@@ -24,34 +25,17 @@ fun rockPaperScissors(movePlayer1: Moves, movePlayer2: Moves): GameState = when 
 
 class RockPaperScissorsTest {
 
-    @Test
-    fun `given I have chosen rock when the opponent chooses scissors then I should win`() {
-        assertEquals(GameState.Player1, rockPaperScissors(Moves.Rock, Moves.Scissors))
+    @TestFactory
+    fun dynamicTestExample() = listOf(
+        Triple(Moves.Rock, Moves.Scissors, GameState.Player1),
+        Triple(Moves.Scissors, Moves.Rock, GameState.Player2),
+        Triple(Moves.Paper, Moves.Rock, GameState.Player1),
+        Triple(Moves.Rock, Moves.Paper, GameState.Player2),
+        Triple(Moves.Scissors, Moves.Paper, GameState.Player1),
+        Triple(Moves.Paper, Moves.Scissors, GameState.Player2),
+    ).map { (movePlayer1, movePlayer2, expected) ->
+        DynamicTest.dynamicTest("given I have chosen $movePlayer1 when the opponent chooses $movePlayer2 then $expected should win") {
+            assertEquals(expected, rockPaperScissors(movePlayer1, movePlayer2))
+        }
     }
-
-    @Test
-    fun `given I have chosen scissors when the opponent chooses rock then the opponent win`() {
-        assertEquals(GameState.Player2, rockPaperScissors(Moves.Scissors, Moves.Rock))
-    }
-
-    @Test
-    fun `given I have chosen paper when the opponent chooses rock then I should win`() {
-        assertEquals(GameState.Player1, rockPaperScissors(Moves.Paper, Moves.Rock))
-    }
-
-    @Test
-    fun `given I have chosen rock when the opponent chooses paper then I should win`() {
-        assertEquals(GameState.Player2, rockPaperScissors(Moves.Rock, Moves.Paper))
-    }
-
-    @Test
-    fun `given I have chosen scissors when the opponent chooses paper then I should win`() {
-        assertEquals(GameState.Player1, rockPaperScissors(Moves.Scissors, Moves.Paper))
-    }
-
-    @Test
-    fun `given I have chosen paper  when the opponent chooses scissors then I should win`() {
-        assertEquals(GameState.Player2, rockPaperScissors(Moves.Paper, Moves.Scissors))
-    }
-
 }
