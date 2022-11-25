5dc8301590b65ccdd628f8410f6e304233bf6378
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-25 18:50:27 +0100

    feat: refactoring primitive obsession to enums

--- kotlin/src/test/kotlin/ExampleTest.kt
+++ kotlin/src/test/kotlin/ExampleTest.kt
@@ -1,32 +1,44 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-fun rockPaperScissors(movePlayer1: String, movePlayer2: String): String {
-    if(movePlayer1 == "rock")
-        return "Player 1";
 
-    if (movePlayer1 == "paper")
-        return "Player 1";
+enum class GameState {
+    Player1,
+    Player2
+}
+
+enum class Moves {
+    Rock,
+    Paper,
+    Scissors
+}
+
+fun rockPaperScissors(movePlayer1: Moves, movePlayer2: Moves): GameState {
+    if(movePlayer1 == Moves.Rock)
+        return GameState.Player1;
+
+    if (movePlayer1 == Moves.Paper)
+        return GameState.Player1;
 
 
-    return "Player 2";
+    return GameState.Player2;
 }
 
 class ExampleTest {
 
     @Test
     fun `given I have chosen rock when the opponent chooses scissors then I should win`() {
-        assertEquals("Player 1", rockPaperScissors("rock", "scissors"))
+        assertEquals(GameState.Player1, rockPaperScissors(Moves.Rock, Moves.Scissors))
     }
 
     @Test
     fun `given I have chosen scissors when the opponent chooses rock then the opponent win`() {
-        assertEquals("Player 2", rockPaperScissors("scissors", "rock"))
+        assertEquals(GameState.Player2, rockPaperScissors(Moves.Scissors, Moves.Rock))
     }
 
     @Test
     fun `given I have chosen paper when the opponent chooses rock then I should win`() {
-        assertEquals("Player 1", rockPaperScissors("paper", "rock"))
+        assertEquals(GameState.Player1, rockPaperScissors(Moves.Paper, Moves.Rock))
     }
 
 }
