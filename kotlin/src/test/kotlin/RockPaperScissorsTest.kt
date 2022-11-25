97c885ba0aca49d3e3a9f9e16f3d49dcd211635d
Félix Díez <felixeu31@gmail.com>
2022-11-25 19:06:13 +0100

    feat: passing test for given I have chosen scissors when the opponent chooses paper then I should win

--- kotlin/src/test/kotlin/RockPaperScissorsTest.kt
+++ kotlin/src/test/kotlin/RockPaperScissorsTest.kt
@@ -17,6 +17,7 @@ fun rockPaperScissors(movePlayer1: Moves, movePlayer2: Moves): GameState = when 
     movePlayer1 == Moves.Rock && movePlayer2 == Moves.Paper -> GameState.Player2
     movePlayer1 == Moves.Rock -> GameState.Player1
     movePlayer1 == Moves.Paper -> GameState.Player1
+    movePlayer1 == Moves.Scissors -> GameState.Player1
     else -> GameState.Player2
 }
 
@@ -40,6 +41,11 @@ class RockPaperScissorsTest {
     @Test
     fun `given I have chosen rock when the opponent chooses paper then I should win`() {
         assertEquals(GameState.Player2, rockPaperScissors(Moves.Rock, Moves.Paper))
+    }
+
+    @Test
+    fun `given I have chosen scissors when the opponent chooses paper then I should win`() {
+        assertEquals(GameState.Player1, rockPaperScissors(Moves.Scissors, Moves.Paper))
     }
 
 }
