14199b4f4a97ff461d173316cd32ab6d57c023f7
Félix Díez <felixeu31@gmail.com>
2022-11-25 18:31:08 +0100

    clean syntax

--- kotlin/src/test/kotlin/ExampleTest.kt
+++ kotlin/src/test/kotlin/ExampleTest.kt
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-fun rockPaperSissors(s: String, s1: String): String {
+fun rockPaperScissors(s: String, s1: String): String {
     return "Player 1";
 }
 
@@ -9,7 +9,7 @@ class ExampleTest {
 
     @Test
     fun `given I have chosen rock when the opponent chooses scissors then I should win`() {
-        assertEquals("Player 1", rockPaperSissors("rock", "sissors"))
+        assertEquals("Player 1", rockPaperScissors("rock", "scissors"))
     }
 
 }
