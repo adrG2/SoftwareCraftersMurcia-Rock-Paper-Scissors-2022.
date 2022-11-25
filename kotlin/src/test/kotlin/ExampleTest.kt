3eae79ee31fc184768d189fea65c65a0834c8cbe
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2022-11-25 18:44:15 +0100

    feat: make the given I have chosen paper when the opponent chooses rock then I should win pass

--- kotlin/src/test/kotlin/ExampleTest.kt
+++ kotlin/src/test/kotlin/ExampleTest.kt
@@ -5,6 +5,10 @@ fun rockPaperScissors(movePlayer1: String, movePlayer2: String): String {
     if(movePlayer1 == "rock")
         return "Player 1";
 
+    if (movePlayer1 == "paper")
+        return "Player 1";
+
+
     return "Player 2";
 }
 
@@ -18,6 +22,11 @@ class ExampleTest {
     @Test
     fun `given I have chosen scissors when the opponent chooses rock then the opponent win`() {
         assertEquals("Player 2", rockPaperScissors("scissors", "rock"))
+    }
+
+    @Test
+    fun `given I have chosen paper when the opponent chooses rock then I should win`() {
+        assertEquals("Player 1", rockPaperScissors("paper", "rock"))
     }
 
 }
